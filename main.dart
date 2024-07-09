8e7d4296d76d55d1d06473704acdfc8e0878b245
btnahmet <137834584+btnahmet@users.noreply.github.com>
2024-07-09 16:43:44 +0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,72 +1,78 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Test());
+  runApp(const WidgetDetail());
 }
-class WidgetDetail extends StatelessWidget {
+
+class WidgetDetail extends StatefulWidget {
   const WidgetDetail({super.key});
 
   @override
+  State<WidgetDetail> createState() => _WidgetDetailState();
+}
+
+class _WidgetDetailState extends State<WidgetDetail> {
+  @override
+  int value = 0;
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar( //baştaki başlık kısmının wigdetini temsil eder
+        appBar: AppBar(
+          //baştaki başlık kısmının wigdetini temsil eder
           title: Text("Baslik"),
           centerTitle: true,
-          backgroundColor: Colors.black54,
+          backgroundColor: Color.fromARGB(137, 3, 65, 73),
           leading: Text("menu"),
-
         ),
         body: Center(
-          child: Text("Hello Flutter"),),
-          floatingActionButton: FloatingActionButton(
-            child: Text("+"),
-           backgroundColor: Colors.blue,
-            onPressed: (){
-
-            },),
-             
-            
-      ),  
-      
-    );
-    
-  }
-}
-
-class Test extends StatefulWidget {
-  const Test({super.key});
-
-  @override
-  State<Test> createState() => _TestState();
-}
-
-class _TestState extends State<Test> {
-  int value = 0;
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar : AppBar(
-                    
+          child: Text(value.toString()),
         ),
-        body:  Center(
-          child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-          Text(value.toString()),
-          ElevatedButton(
-            onPressed:  (){
+        floatingActionButton: FloatingActionButton(
+          child: Text("+"),
+          backgroundColor: Colors.blue,
+          onPressed: () {
+            setState(() {
               value += 1;
-              print(value);
-            },
-            child: Text("Tikla"),
-            )
-        ],
+            });
+          },
         ),
-        ),
-      ), 
-      
+      ),
     );
   }
 }
+
+// class Test extends StatefulWidget {
+//   const Test({super.key});
+
+//   @override
+//   State<Test> createState() => _TestState();
+// }
+
+// class _TestState extends State<Test> {
+//   int value = 0;
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(),
+//         body: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Text(value.toString()),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   setState(() {
+//                     value += 1;
+//                     print(value);
+//                   });
+//                 },
+//                 child: Text("Tikla"),
+//               )
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
