44aff541e17279409005429d68df6fd0303498fa
btnahmet <137834584+btnahmet@users.noreply.github.com>
2024-07-11 16:31:11 +0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart'; // renk paketi
 
@@ -19,46 +22,116 @@ class _WidgetDetailState extends State<WidgetDetail> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(textTheme: GoogleFonts.acmeTextTheme()),
-      home: Scaffold(
-        appBar: AppBar(
-          //baştaki başlık kısmının wigdetini temsil eder
-          title: const Text("Baslik"),
-          centerTitle: true,
-          backgroundColor: const Color.fromARGB(137, 3, 65, 73),
-          leading: const Text("menu"),
-        ),
-        body: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(value.toString()),
-            Text(
-              "Ahmet",
-              style: TextStyle(
-                fontSize: 40, // Textin genel büyüklüğünü temsil eder
-                fontWeight: FontWeight.w500, // harf kalınlığını temsil eder
-                letterSpacing: 20, // harfler arası boşluğu temsil eder
-                color: HexColor("525FE1"),
-                backgroundColor: Colors.blue,
-              ),
-            )
-          ],
-        )),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.blue,
-          onPressed: () {
-            setState(() {
-              value += 1;
-            });
-          },
-          child: const Text("+"),
-        ),
-      ),
-    );
-  }
-}
+        theme: ThemeData(
+            textTheme:
+                GoogleFonts.acmeTextTheme()), // butun sayfayi bu fontla yazar
+        // home: Row(
+        //   children: [
+        //     Flexible(
+        //       flex: 3,
+        //       child:  Container(
+        //       color:const Color.fromARGB(255, 168, 90, 90),
+        //       width: 500,
+        //       height: 100,
 
+        //     ),
+        //     ),
+
+        //     Flexible(
+        //       flex: 1,
+        //       child: Container(
+        //       color: Colors.red,
+        //        width: 200,
+        //       height: 100,
+        //     ),)
+        //   ],
+        // )
+
+        home: Row(
+          children: [
+            Expanded(
+              child: 
+              Container(
+                child :Text("kfdmvlknmdfkdfklnbkfldnfdnbklfdnkbllfdnkbldfnkbklfdlv"),
+                color: const Color.fromARGB(255, 168, 90, 90),
+                width: 500,
+                //height: 100,
+              ),
+            ),
+
+            // Expanded(
+
+            // child:
+            Container(
+              color: Colors.red,
+              width: 200,
+              height: 100,
+            ),
+            //)
+          ],
+        )
+
+        /////////////////////////////////////
+        // Scaffold(
+        //   appBar: AppBar(
+        //     //baştaki başlık kısmının wigdetini temsil eder
+        //     title: const Text("Baslik"),
+        //     centerTitle: true,
+        //     backgroundColor: Color.fromARGB(136, 5, 80, 90),
+        //     leading: const Text("menu"),
+        //   ),)
+
+        /////////////////////////////////////
+        // body: Container(
+        //padding: EdgeInsets.all(15.0), // .all her yerinden genişletmeyi sağlar
+        //padding: EdgeInsets.fromLTRB(left, top, right, bottom), //görülen sıralamada genişletmeyi sağlar
+        //padding: EdgeInsets.only(left: 15.0), // sadece tek bir noktadan boşluk bıraktırır.
+        //margin: EdgeInsets.all(15.0),
+        // color: Colors.green,  //sadece bu satır varken container bir üst yani body kadar yer kaplar.
+        // child: const Text("Ahmet"), // ancak container içine bir widget geldikten sonra container bu alan kadar yer kaplar
+        // ),
+        // ///////////////////////////////////
+        // body: Row(
+        //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,  //yatayda hizalar
+        //   crossAxisAlignment: CrossAxisAlignment.start,   // dikeyde hizalar
+        //   children: [
+        //   Text("Ahmet"),
+        //   FloatingActionButton(onPressed: (){} , child: Text("buton")),
+        //   Container(
+        //     child: Text("Yesil"),
+        //     color: Colors.blue,
+        //     padding: EdgeInsets.all(15.0),
+        //   )
+        // ],),
+        // ///////////////////////////////////
+        //   body: Column(
+        //   mainAxisAlignment: MainAxisAlignment.start,  //dikeyde hizalar
+        //   crossAxisAlignment: CrossAxisAlignment.center,   //yatayda  hizalar
+        //   children: [
+        //   Text("Ahmet"),
+        //   FloatingActionButton(onPressed: (){} , child: Text("buton")),
+        //   Container(
+        //     child: Text("Yesil"),
+        //     color: Colors.blue,
+        //     padding: EdgeInsets.all(35.0),
+        //   )
+        // ],),
+        // ///////////////////////////////////////////
+
+//         floatingActionButton: FloatingActionButton(
+//           backgroundColor: Colors.blue,
+//           onPressed: () {
+//             setState(() {
+//               value += 1;
+//             });
+//           },
+//           child: const Text("+"),
+//         ),
+//       ),
+//     );
+//   }
+// }
+////////////////////////////////////////////////////////////////////////////////
 // class Test extends StatefulWidget {
 //   const Test({super.key});
 
@@ -91,6 +164,6 @@ class _WidgetDetailState extends State<WidgetDetail> {
 //           ),
 //         ),
 //       ),
-//     );
-//   }
-// }
+        );
+  }
+}
